3d97d6acd3793f0de99e72a0dd94197a41e11172
Anas <anaselnahef12@gmail.com>
2022-04-13 20:43:00 +0200

    add the header

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,36 @@
 <template>
-  <div></div>
+  <div class="container">
+    <the-header></the-header>
+  </div>
 </template>
 
 <script>
-export default {};
+import TheHeader from "./components/TheHeader.vue";
+export default {
+  components: {
+    TheHeader,
+  },
+};
 </script>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
 #app {
-  font-family: "Roboto", sans-serif;
+  font-family: "Plus Jakarta Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+@media only screen and (min-width: 1200px) {
+  .container {
+    max-width: 80%;
+    margin: 0 auto;
+  }
 }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,103 @@
+<template>
+  <header>
+    <div class="col-1 logo">
+      <img src="../assets/logo.svg" alt="Nike logo" width="60" height="60" />
+    </div>
+
+    <ul class="col-2 center">
+      <li>new</li>
+      <li>Men</li>
+      <li>women</li>
+      <li>kids</li>
+      <li class="offer">special offers</li>
+    </ul>
+    <ul class="col-3 aside">
+      <li><i class="fa-solid fa-magnifying-glass"></i></li>
+      <li><i class="fa-regular fa-heart"></i></li>
+      <li><i class="fa-solid fa-cart-shopping"></i></li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+header {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  align-content: stretch;
+  height: 4rem;
+  background: #fff;
+  overflow: hidden;
+}
+.col-1 {
+  flex: 20%;
+  text-align: center;
+}
+.col-2 {
+  flex: 60%;
+}
+.col-3 {
+  flex: 20%;
+}
+.center {
+  list-style-type: none;
+  display: flex;
+  align-items: center;
+  height: 100%;
+}
+.center li {
+  padding: 0 20px;
+  height: inherit;
+  line-height: 4rem;
+  vertical-align: middle;
+  font-weight: 500;
+  font-size: 130%;
+  text-transform: capitalize;
+  white-space: nowrap;
+  transition: background 0.5s ease;
+}
+.center li:hover {
+  background: #f3f3f3;
+  cursor: pointer;
+}
+.offer {
+  color: rgb(247, 79, 67);
+}
+.aside {
+  display: flex;
+  justify-content: center;
+  column-gap: 20px;
+  list-style-type: none;
+}
+.aside li {
+  padding: 10px 10px;
+}
+.aside li:hover {
+  background: #f3f3f3;
+  border-radius: 50%;
+  cursor: pointer;
+}
+.icon {
+  display: none;
+  font-size: 150%;
+  margin-right: 10px;
+  width: 10px;
+  height: 10px;
+  position: absolute;
+  top: 10px;
+  right: 20px;
+  padding: 10px;
+}
+
+.icon i:hover {
+  border-radius: 50%;
+  cursor: pointer;
+  transition: all 0.6s ease;
+  transform: rotate(90deg);
+}
+</style>
